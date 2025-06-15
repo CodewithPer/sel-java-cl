a76473ee53c2c5ccf4b1a9999a8e796d24922ea5
Chandula Perera <chandulaperera@Chandulas-MacBook-Air.local>
2025-06-15 23:42:52 +1200

    Initial commit

---
+++ src/test/java/com/example/GoogleTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GoogleTest {
+
+    @Test
+    public void testGoogleTitle() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        assertTrue(driver.getTitle().contains("Google"));
+        driver.quit();
+    }
+}
